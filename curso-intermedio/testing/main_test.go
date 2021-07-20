e21c89a743bd06fbc23ced6ce9036f0e1863c50a
Ariel Gugliotta <arielgugliotta89@gmail.com>
2021-07-20 17:14:27 -0300

    Code coverage 100%

--- curso-intermedio/testing/main_test.go
+++ curso-intermedio/testing/main_test.go
@@ -24,3 +24,22 @@ func TestSum(t *testing.T) {
 		}
 	}
 }
+
+func TestGetMax(t *testing.T) {
+	tables := []struct {
+		a int
+		b int
+		n int
+	}{
+		{7, 9, 9},
+		{5, 4, 5},
+		{25, 26, 26},
+	}
+
+	for _, item := range tables {
+		max := GetMax(item.a, item.b)
+		if max != item.n {
+			t.Errorf("Error en el test de Get Max. Tuvimos un resultado de %d se esperaba %d", max, item.n)
+		}
+	}
+}
