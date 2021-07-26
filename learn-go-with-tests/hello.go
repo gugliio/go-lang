dd83b0cff2af940eef8b766971c6d8a3aec7f9b9
Ariel Gugliotta <arielgugliotta89@gmail.com>
2021-07-26 17:39:42 -0300

    Testing - Hello Name with Empty Name returns Hello, World

--- learn-go-with-tests/hello.go
+++ learn-go-with-tests/hello.go
@@ -2,14 +2,19 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func main() {
 	fmt.Println(Hello())
 }
 
 func Hello() string {
-	return "Hello, world"
+	return englishHelloPrefix + "world"
 }
 
 func HelloName(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
--- learn-go-with-tests/hello_test.go
+++ learn-go-with-tests/hello_test.go
@@ -12,10 +12,27 @@ func TestHello(t *testing.T) {
 }
 
 func TestHelloName(t *testing.T) {
-	got := HelloName("Ariel")
-	want := "Hello, Ariel"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	// For helper functions, it's a good idea to accept a testing.TB which is an interface that *testing.T
+	// and *testing.B both satisfy, so you can call helper functions from a test, or a benchmark.
+
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("Say Hello with name", func(t *testing.T) {
+		got := HelloName("Ariel")
+		want := "Hello, Ariel"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say Hello without name", func(t *testing.T) {
+		got := HelloName("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
