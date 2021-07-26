6176748fccba6cd731c3e391d1a5bcb6484fbfae
Ariel Gugliotta <arielgugliotta89@gmail.com>
2021-07-26 09:43:24 -0300

    Manejo de slices con append

--- golang-server/lista-de-tareas/tareas.go
+++ golang-server/lista-de-tareas/tareas.go
@@ -8,6 +8,18 @@ type tasks struct {
 	Completado  bool
 }
 
+type taskList struct {
+	tasks []*tasks
+}
+
+func (t *taskList) agregarALista(task *tasks) {
+	t.tasks = append(t.tasks, task)
+}
+
+func (t *taskList) eliminarDeLista(index int) {
+	t.tasks = append(t.tasks[:index], t.tasks[index+1:]...)
+}
+
 func (t *tasks) marcarCompleta() {
 	t.Completado = true
 }
@@ -21,11 +33,18 @@ func (t *tasks) actualizarNombre(nombre string) {
 }
 
 func main() {
-	t := &tasks{Nombre: "Completar curso", Descripcion: "Completar curso en una semana"}
-	fmt.Printf("%+v\n", t)
+	t1 := &tasks{Nombre: "Completar curso Golang", Descripcion: "Completar curso en una semana"}
+	t2 := &tasks{Nombre: "Completar curso Python", Descripcion: "Completar curso en una semana"}
 
-	t.marcarCompleta()
-	t.actualizarNombre("Finalizar curso")
-	t.actualizarDescripcion("Finalizar curso lo antes posible")
-	fmt.Printf("%+v\n", t)
+	listaTareas := &taskList{
+		tasks: []*tasks{t1, t2},
+	}
+
+	fmt.Println(listaTareas.tasks[0])
+
+	t3 := &tasks{Nombre: "Completar curso Node Js", Descripcion: "Completar curso en una semana"}
+	listaTareas.agregarALista(t3)
+	fmt.Println(len(listaTareas.tasks))
+	listaTareas.eliminarDeLista(1)
+	fmt.Println(len(listaTareas.tasks))
 }
