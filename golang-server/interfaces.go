10d223278e05359491612d68917e4f5cb6391efe
Ariel Gugliotta <arielgugliotta89@gmail.com>
2021-07-28 20:36:44 -0300

    Golang Server - Interfaces Animal

---
+++ golang-server/interfaces.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type animal interface {
+	mover() string
+}
+
+type perro struct {
+}
+
+type pez struct {
+}
+
+type pajaro struct {
+}
+
+func (perro) mover() string {
+	return "soy un perro y camino"
+}
+
+func (pez) mover() string {
+	return "soy un pez y nado"
+}
+
+func (pajaro) mover() string {
+	return "soy un pajaro y estoy volando"
+}
+
+func moverAnimal(a animal) {
+	fmt.Println(a.mover())
+}
+
+func main() {
+	p := perro{}
+	moverAnimal(p)
+	pe := pez{}
+	moverAnimal(pe)
+	pa := pajaro{}
+	moverAnimal(pa)
+}
