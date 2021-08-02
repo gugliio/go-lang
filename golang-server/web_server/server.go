85dfe33d24f63420556222879e2aacd6696fedf4
Ariel Gugliotta <arielgugliotta89@gmail.com>
2021-08-02 19:36:13 -0300

    Go server - Hello world port :3000

--- golang-server/web_server/server.go
+++ golang-server/web_server/server.go
@@ -3,14 +3,19 @@ package main
 import "net/http"
 
 type Server struct {
-	port string
+	port   string
+	router *Router
 }
 
 func NewServer(port string) *Server {
-	return &Server{port: port}
+	return &Server{
+		port:   port,
+		router: NewRouter(),
+	}
 }
 
 func (s *Server) Listen() error {
+	http.Handle("/", s.router)
 	err := http.ListenAndServe(s.port, nil)
 	if err != nil {
 		return err
