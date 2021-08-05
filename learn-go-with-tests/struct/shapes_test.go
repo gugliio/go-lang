a721d2cd25a7e25b8ed8da2858378e6dec2e505b
Ariel Gugliotta <arielgugliotta89@gmail.com>
2021-08-04 21:55:15 -0300

    Go test - Metodos, interfaces y structs completado

--- learn-go-with-tests/struct/shapes_test.go
+++ learn-go-with-tests/struct/shapes_test.go
@@ -12,6 +12,7 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
+/*
 func TestArea(t *testing.T) {
 
 	checkArea := func(t testing.TB, shape Shape, want float64) {
@@ -33,4 +34,26 @@ func TestArea(t *testing.T) {
 		want := 314.1592653589793
 		checkArea(t, circle, want)
 	})
+}
+*/
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name: "circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "triangle", shape: Triangle{12, 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("Se obtuvo %g y se esperaba un area de %g", got, tt.hasArea)
+			}
+		})
+	}
 }
