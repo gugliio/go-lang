2f17dc06f648558d217031314d5e6a826df91cf7
Ariel Gugliotta <arielgugliotta89@gmail.com>
2021-08-04 21:39:52 -0300

    Go Test - TDD Methods

---
+++ learn-go-with-tests/struct/shapes.go
@@ -0,0 +1,24 @@
+package structure
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radio float64
+}
+
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.Height + r.Width)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radio * c.Radio
+}
