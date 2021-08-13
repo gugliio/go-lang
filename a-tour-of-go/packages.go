0d2754dbaa3535b2b9a96a49e9a448e620789fbf
Ariel Gugliotta <arielgugliotta89@gmail.com>
2021-08-13 10:00:00 -0300

    A Tour of Go - Packages - Funcion no exportada

--- a-tour-of-go/packages.go
+++ a-tour-of-go/packages.go
@@ -6,10 +6,16 @@ import (
 	"math/rand"
 )
 
+func add(a, b int) int {
+	return a + b
+}
+
 func main() {
 	fmt.Println("Random number using math/rand package", rand.Intn(100))
 	fmt.Printf("Raiz cuadrada de un numero %g test\n", math.Sqrt(49))
 
 	//Exported name begins with capital letter (math.Pi)
 	fmt.Println("Pi number", math.Pi)
+
+	fmt.Println("Funcion con parametros no exportada", add(42, 13))
 }
