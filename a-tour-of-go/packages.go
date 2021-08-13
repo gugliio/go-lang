aa2d12c7884ded3e78e060b1ab8d8e2129508465
Ariel Gugliotta <arielgugliotta89@gmail.com>
2021-08-13 09:56:37 -0300

    A Tour of Go - Packages - Math package

--- a-tour-of-go/packages.go
+++ a-tour-of-go/packages.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
 func main() {
 	fmt.Println("Random number using math/rand package", rand.Intn(100))
+	fmt.Printf("Raiz cuadrada de un numero %g test\n", math.Sqrt(49))
+
 }
