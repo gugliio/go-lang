327d994d28d10182a89a4ea841625eb31de5f389
Ariel Gugliotta <arielgugliotta89@gmail.com>
2021-08-13 09:58:44 -0300

    A Tour of Go - Packages - Exported names

--- a-tour-of-go/packages.go
+++ a-tour-of-go/packages.go
@@ -10,4 +10,6 @@ func main() {
 	fmt.Println("Random number using math/rand package", rand.Intn(100))
 	fmt.Printf("Raiz cuadrada de un numero %g test\n", math.Sqrt(49))
 
+	//Exported name begins with capital letter (math.Pi)
+	fmt.Println("Pi number", math.Pi)
 }
