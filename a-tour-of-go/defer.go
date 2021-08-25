fda407b8ffd99374c716a399a4366e90ed83bd0c
Ariel Gugliotta <arielgugliotta89@gmail.com>
2021-08-25 12:01:02 -0300

    A Tour of GO - Defer

--- a-tour-of-go/defer.go
+++ a-tour-of-go/defer.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	defer fmt.Println("mundo")
-	fmt.Println("Hola")
+	/*
+			defer fmt.Println("mundo")
+		   	fmt.Println("Hola")
+	*/
+
+	fmt.Println("continua")
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("Termino")
 }
