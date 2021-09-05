6595160733fe48129b7b802098de5136264f50d0
Ariel Gugliotta <arielgugliotta89@gmail.com>
2021-09-05 10:12:14 -0300

    A Tour of Go - Indirection Values

--- a-tour-of-go/methods/methods-pointer.go
+++ a-tour-of-go/methods/methods-pointer.go
@@ -13,6 +13,10 @@ func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+func AbsFunc(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
 func (v *Vertex) Scale(f float64) {
 	v.X = v.X * f
 	v.Y = v.Y * f
@@ -33,4 +37,12 @@ func main() {
 	fmt.Println("First ABS", v2.Abs())
 	Scale2(&v2, 10)
 	fmt.Println("Second ABS", v2.Abs())
+
+	v3 := Vertex{3, 4}
+	fmt.Println(v3.Abs())
+	fmt.Println(AbsFunc(v3))
+
+	p := &Vertex{4, 3}
+	fmt.Println(p.Abs())
+	fmt.Println(AbsFunc(*p))
 }
