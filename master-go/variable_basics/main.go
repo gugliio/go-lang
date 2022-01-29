b2173e0727d47d415af56b1e09deb06ff3226922
Ariel Gugliotta <arielgugliotta89@gmail.com>
2022-01-29 19:51:06 -0300

    Master Go - Go basics - Multiple declarations

--- master-go/variable_basics/main.go
+++ master-go/variable_basics/main.go
@@ -15,4 +15,33 @@ func main() {
 	name = "Andrei"
 	name1 := "John"
 	_ = name1
+
+	car, cost := "Audi", 5000
+	fmt.Println(car, cost)
+	car, year := "BMW", 2006
+	_ = year
+
+	var opened = false
+	opened, file := true, "a.txt"
+	_, _ = opened, file
+
+	var (
+		salary    float64
+		firstName string
+		gender    string
+	)
+	fmt.Println(salary, firstName, gender)
+
+	var a, b, c int
+	fmt.Println(a, b, c)
+
+	var i, j int
+	i, j = 5, 8
+
+	_, _ = i, j
+
+	fmt.Println(i, j)
+
+	sum := 5 + 2.3
+	fmt.Println(sum)
 }
