4c3cbd446cb28d971b517bd5ddbe1e36e02b08e8
Ariel Gugliotta <arielgugliotta89@gmail.com>
2022-01-29 19:47:07 -0300

    Master Go - Go basics - Variables

---
+++ master-go/variable_basics/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int = 30
+	fmt.Println("Age:", age)
+
+	var name = "Dan"
+	_ = name
+
+	s := "Learing golang"
+	fmt.Println(s)
+
+	name = "Andrei"
+	name1 := "John"
+	_ = name1
+}
